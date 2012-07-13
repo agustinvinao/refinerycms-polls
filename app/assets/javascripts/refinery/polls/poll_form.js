7d3c75937080e948b3ef004d7918c39baf9ef9da
Agustin Viñao <agustinvinao@gmail.com>
2012-07-13 11:44:57 -0300

    translate js to coffee script

--- app/assets/javascripts/refinery/polls/poll_form.js
+++
@@ -1,8 +0,0 @@
-function do_vote(element) {
-  $(element).parents().find('form').submit();
-}
-$(document).ready(function(){
-  $("input[type=radio][name=answer_id]").click(function() {
-    do_vote(this);
-  });
-})
