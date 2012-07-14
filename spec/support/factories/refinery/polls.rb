7ef8c87d0c41f403dd2d126444394a429ea968c1
Agustin Viñao <agustinvinao@gmail.com>
2012-07-14 20:02:31 -0300

    Fix tests for Question model

--- spec/support/factories/refinery/polls.rb
+++ spec/support/factories/refinery/polls.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
   factory :question, :class => Refinery::Polls::Question do
     sequence(:title) { |n| "refinery#{n}" }
   end
+  factory :answer, :class => Refinery::Polls::Answer do
+    sequence(:title) { |n| "refinery-answer#{n}" }
+    question  nil
+  end
 end
 
